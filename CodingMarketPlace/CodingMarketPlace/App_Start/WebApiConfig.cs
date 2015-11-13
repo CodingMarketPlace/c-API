946b6794374db82b2c10c877f5da0db196a903bb
DevilBapt <badmib40@hotmail.fr>
2015-11-13 14:27:03 +0100

    correction webapiconfig.cs

--- CodingMarketPlace/CodingMarketPlace/App_Start/WebApiConfig.cs
+++ CodingMarketPlace/CodingMarketPlace/App_Start/WebApiConfig.cs
@@ -21,7 +21,7 @@ namespace CodingMarketPlace
                 defaults: new { id = RouteParameter.Optional }
             );
 
-            var corsAttr = new EnableCorsAttribute("*", "*", "*");
+            var corsAttr = new EnableCorsAttribute("http://codingmarketplace.herokuapp.com", "*", "*");
             config.EnableCors(corsAttr);
         }
     }
